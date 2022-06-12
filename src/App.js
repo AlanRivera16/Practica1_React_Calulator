12cffb21d8d6a65ee1f57dacf9d5b53a2124d32c
AlanRivera16 <rivera.alan.3m@gmail.com>
2022-06-11 21:49:46 -0500

    style: added styles in the component of ol, inputs and buttons as well as images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import image from "./sources/img-user.jpg"
+import React, {useState} from "react";
 
+//Cualquier lógica aquí afuera será global 
 function App() {
+  // const [counter, setCounter] = useState(0);
+  const [people, setPeople] = useState([
+    {
+      name: "John",
+      lastName: "Doe",
+      age: 30,
+      favouriteFood: "Pizza",
+      favouriteColour: "Red",
+      counter: 0,
+    },
+  ]);
+  const [name, setName] = useState("");
+  const [lastName, setLastname] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      
+    <div>
+      <div><img className='img-back' src={image} alt="USERIMG"/></div>
+      <div className='d-block-custom'>
+        <input
+          type="text"
+          name="name"
+          className='input-custom'
+          placeholder='Inserta el nombre'
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          name="lastName"
+          className='input-custom'
+          placeholder='Inserta el apellido'
+          onChange={(event) => {
+            setLastname(event.target.value);
+          }}
+        />
+      </div>
+        <button
+          onClick={() => {
+            setPeople((current) => [{ name, lastName }, ...current]);
+          }}
+          className='btn-agregar-user'
         >
-          Learn React
-        </a>
-      </header>
+          AGREGAR USUARIO
+        </button>
     </div>
+    
+
+    <div id='Hello'>
+      <div className='m-custom'>
+        <div className='bg-dark'><h1>TABLA CONTENIDO</h1></div>
+        <div>
+          <ol className='pr-20'>
+            <div className='d-flex mb-21'><p className='w-50'>NOMBRE</p><p className='w-50'>APELLIDO</p></div>
+            {people.map((person, idx) => (
+            <li className='border-y' id={idx}><div className='d-flex'><p className='w-50'>{`${person.name}`}</p><p className='w-50'>{`${person.lastName}`}</p></div></li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    </div>
+
+    
+  </div>
+  
   );
 }
 
