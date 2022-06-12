8b27bf52fc404a267a9112857570d4a0595df193
AlanRivera16 <rivera.alan.3m@gmail.com>
2022-06-12 16:14:56 -0500

    feat: it was added the elements (table, buttons, inputs) to manage the functions of a calculator and the data

--- src/App.js
+++ src/App.js
@@ -18,6 +18,18 @@ function App() {
   const [name, setName] = useState("");
   const [lastName, setLastname] = useState("");
 
+  //const para input calculator
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+
+  // const [numbers, setNumbers] = useState([
+  //   {
+  //     number1: 0,
+  //     number: 2
+  //   },
+  // ]);
+  const [result, setResult] = useState(0);
+
   return (
     <div className="App">
       
@@ -64,6 +76,119 @@ function App() {
             <li className='border-y' id={idx}><div className='d-flex'><p className='w-50'>{`${person.name}`}</p><p className='w-50'>{`${person.lastName}`}</p></div></li>
             ))}
           </ol>
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <div><img className='img-back' src={image} alt="USERIMG"/></div>
+      <div className='d-block-custom shadow'>
+        <input
+          type="number"
+          name="number1"
+          className='input-custom'
+          placeholder='numero 1'
+          onChange={(event) => {
+            setNum1(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          name="number2"
+          className='input-custom'
+          placeholder='numero 2'
+          onChange={(event) => {
+            setNum2(event.target.value);
+          }}
+        />
+      </div>
+      <div className='shadow'>
+        <button
+          onClick={() => {
+            setResult(((+num1)+(+num2)));
+          }}
+          className='btn-agregar-user'
+        >
+          Sumar
+        </button>
+        <button
+          onClick={() => {
+            setResult(((+num1)-(+num2)));
+          }}
+          className='btn-agregar-user'
+        >
+          Restar
+        </button>
+        <button
+          onClick={() => {
+            setResult(((+num1)*(+num2)));
+          }}
+          className='btn-agregar-user'
+        >
+          Multiplicar
+        </button>
+        <button
+          onClick={() => {
+            if(num2 == 0){
+              console.log('No se puede dividir entre 0');
+              window.alert('No se puede dividir entre 0, selecciona un número válido')
+              return;
+            }
+            setResult(((+num1)/(+num2)));
+          }}
+          className='btn-agregar-user'
+        >
+          Dividir
+        </button>
+      </div>
+        <p>{result}</p>
+    </div>
+
+    
+    <div id='Hello'>
+      <div className='m-custom'>
+        <div>
+          <table className='w-100'>
+            <tr>
+              {/* <td colspan="4">0</td> */}
+            </tr>
+            <tr>
+              <td>
+                  <button
+                    onClick={() => {
+                      setResult(((+num1) + (+num2)));
+                    }}
+                    className='btn-number'
+                  >
+                    1
+                  </button>
+              </td>
+              <td>2</td>
+              <td>3</td>
+              <td>+</td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td>5</td>
+              <td>6</td>
+              <td>-</td>
+            </tr>
+            <tr>
+              <td>7</td>
+              <td>8</td>
+              <td>9</td>
+              <td>*</td>
+            </tr>
+            <tr>
+              <td colspan="3">0</td>
+              <td>/</td>
+            </tr>
+            <tr>
+              <td colspan="2">AC</td>
+              <td>.</td>
+              <td>=</td>
+            </tr>
+          </table>
         </div>
       </div>
     </div>
